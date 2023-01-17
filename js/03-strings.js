94fbfc2663ba32b2cf4c97fcbf4a26458b730043
Katažina <katazina.fukalova@gmail.com>
2023-01-18 00:28:13 +0200

    Homework

---
+++ js/03-strings.js
@@ -0,0 +1,16 @@
+const spalva = 'geltona';
+console.log(spalva)
+const spalva1 = 'balta';
+console.log(spalva1)
+const  spalva2 = 'melyna';
+console.log(spalva2)
+const  spalva3 = 'ruda';
+console.log(spalva3)
+const spalva4 = 'juoda';
+console.log(spalva4)
+
+const visi = spalva + ' ' + spalva1 + ' ' + spalva2 + ' ' + spalva3 + ' ' + spalva4;
+console.log(visi);
+
+const visi2 = spalva4 + ', ' + spalva3 + ', ' + spalva2 + ', ' + spalva1 + ', ' + spalva;
+console.log(visi2);
---
+++ js/06-if.js
@@ -0,0 +1,23 @@
+if (7 == 7) {
+    console.log('pomidoras');
+} else {
+    console.log('bandykite dar karta');
+}
+
+if (4 > 5) {
+    console.log('mazesnis');
+} else {
+    console.log('didesnis');
+}
+
+if (4 == 5) {
+    console.log('lygu');
+} else {
+    console.log('nelygu');
+}
+
+if (4 <= 5) {
+    console.log('didesnis');
+} else {
+    console.log('mazesnis');
+}
