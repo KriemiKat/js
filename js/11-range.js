b73e6e8f5d0da4fccba8071c59d2d04631abf8c7
Katažina <katazina.fukalova@gmail.com>
2023-01-25 23:54:46 +0200

    code changes

--- js/11-range.js
+++ js/11-range.js
@@ -1,8 +1,7 @@
-function  rangeSum() {
+/* function  rangeSum() {
     return 0;
 }
 
-/*
 0...0   0
 0...4   10
 0...100  5050
@@ -14,3 +13,66 @@ console.log(rangeSum(0, 0), '-->', 0);
 console.log(rangeSum(0, 100), '-->', 5050);
 console.log(rangeSum(-50, 50), '-->', 0);
 console.log(rangeSum(0, 0), '-->', 0);
+
+function removeChar(str){
+  removeChar.slice = str.slice(1,-1)
+        
+    }
+
+   
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Tests", () => {
+  it("Fixed Tests", () => {
+    assert.strictEqual(removeChar('eloquent'), 'loquen');
+    assert.strictEqual(removeChar('country'), 'ountr');
+    assert.strictEqual(removeChar('person'), 'erso');
+    assert.strictEqual(removeChar('place'), 'lac');
+    assert.strictEqual(removeChar('ooopsss'), 'oopss');
+  });
+}
+*/
+/*console.clear();
+
+const amzius = [2, 1, 4, 9, 5, 7];
+
+console.log(Math.min(...amzius));
+console.log(Math.max(...amzius));*/
+
+
+
+let text = "How are you doing today?";
+const myArray = text.split(" ");
+console.log(myArray);
+
+const reversed = myArray.reverse();
+console.log(myArray);
+
+let text2 = reversed.join;
+console.log(text2);
+
+/*
+<p>The second word is:</p>
+<p id="demo"></p>
+
+<script>
+let text = "How are you doing today?";
+const myArray = text.split(" ");
+
+document.getElementById("demo").innerHTML = myArray[1]; 
+</script>*/
+
+
+{
+  if (operation === '+') {
+    return value1 + value2;
+    } else if (operation ==='-'){
+     return value1 - value2;}
+     else if (operation === '*'){
+     return value1 * value2;}
+    else if (operation === '/'){
+     return value1 - value2;
+ }
+ }
